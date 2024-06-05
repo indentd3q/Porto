fa63f3aee1a157c769734802fb671c1d6662da45
indentd3q <vincentzq88@gmail.com>
2024-06-05 22:25:46 +0700

    Tambahin Footer

--- reactporto/src/App.jsx
+++ reactporto/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     gsap.to('header h1', { duration: 1, opacity: 0 });
     gsap.to('main', { duration: 1, y: -100, ease: 'bounce' });
     gsap.to('footer', { duration: 1, y: 100, ease: 'bounce' });
+    gsap.to('footer p', { duration: 1, opacity: 0 });
   }, []);
 
   return (
