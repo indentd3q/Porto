c5c53619bcdbe9debddd459d5040e2eb4bbda92b
indentd3q <vincentzq88@gmail.com>
2024-06-05 22:23:29 +0700

    Stress Masih Error

--- reactporto/src/App.js
+++ reactporto/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
+  useEffect(() => {
+    gsap.to('header', { duration: 1, y: -100, ease: 'bounce' });
+    gsap.to('header h1', { duration: 1, opacity: 0 });
+    gsap.to('main', { duration: 1, y: -100, ease: 'bounce' });
+    gsap.to('footer', { duration: 1, y: 100, ease: 'bounce' });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main className="container mx-auto p-4">
+        <About />
+        <Projects />
+        <Blog />
+        <Contact />
+      </main>
+      <Footer />
     </div>
   );
 }
