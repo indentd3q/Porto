15abb7b07576db4475f2d3088a8e8f9ac839c345
indentd3q <vincentzq88@gmail.com>
2024-06-08 21:00:32 +0700

    hehe update desgin

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -4,7 +4,7 @@ const About = () => (
   <section id="about" className="bg-white py-20">
     <h2 className="text-3xl font-bold mb-4">Tentang Aing Suree</h2>
     <p className="text-lg">
-    Undergraduate Computer Science Student at Universitas Multimedia Nusantara
+    Update Baru OTW
     </p>
   </section>
 );
