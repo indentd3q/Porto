56621d89c224c303bc6bf8b09a8f1649a772f5a6
indentd3q <vincentzq88@gmail.com>
2024-06-15 23:51:06 +0700

    Update Blog (Template)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './components/Landing';
 import About from './components/About';
+import Blog from './components/Blog';
 import { useAnimations } from './animations';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
                 <Routes>
                     <Route exact path="/" element={<Landing />} />
                     <Route path="/about" element={<About />} />
+                    <Route path="/blog" element={<Blog />} />
                 </Routes>
             </div>
         </Router>
--- my-app/src/components/Blog.js
+++ my-app/src/components/Blog.js
@@ -1,20 +1,40 @@
 import React from 'react';
 
+const blogPosts = [
+  {
+    id: 1,
+    title: "Blog Post 1",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.",
+    image: "blog1.jpg",
+    link: "#",
+  },
+  {
+    id: 2,
+    title: "Blog Post 2",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.",
+    image: "blog2.jpg",
+    link: "#",
+  },
+  // Add more blog posts here
+];
+
 const Blog = () => (
   <section id="blog" className="bg-white py-20">
-    <h2 className="text-3xl font-bold mb-4">My Blog</h2>
-    <div className="flex flex-wrap justify-center">
-      <div className="blog-post w-1/2 md:w-1/3 xl:w-1/4 p-4">
-        <img src="blog1.jpg" alt="Blog Post 1" className="w-full h-48 object-cover rounded" />
-        <h3 className="text-lg font-bold mb-2">Blog Post 1</h3>
-        <p className="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.</p>
-        <a href="#" className="text-lg font-bold text-indigo-600 hover:text-indigo-900">Read More</a>
-      </div>
-      <div className="blog-post w-1/2 md:w-1/3 xl:w-1/4 p-4">
-        <img src="blog2.jpg" alt="Blog Post 2" className="w-full h-48 object-cover rounded" />
-        <h3 className="text-lg font-bold mb-2">Blog Post 2</h3>
-        <p className="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.</p>
-        <a href="#" className="text-lg font-bold text-indigo-600 hover:text-indigo-900">Read More</a>
+    <div className="container mx-auto">
+      <h2 className="text-4xl font-bold text-center mb-12">My Blog</h2>
+      <div className="flex flex-wrap justify-center">
+        {blogPosts.map((post) => (
+          <div key={post.id} className="blog-post w-full sm:w-1/2 md:w-1/3 xl:w-1/4 p-4">
+            <div className="bg-gray-100 rounded-lg overflow-hidden shadow-lg">
+              <img src={post.image} alt={post.title} className="w-full h-48 object-cover" />
+              <div className="p-4">
+                <h3 className="text-lg font-bold mb-2">{post.title}</h3>
+                <p className="text-gray-700 mb-4">{post.description}</p>
+                <a href={post.link} className="text-indigo-600 hover:text-indigo-900 font-bold">Read More</a>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   </section>
