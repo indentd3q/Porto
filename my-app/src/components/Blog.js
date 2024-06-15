5ffe1dbd8027dbe79ab41703202147398af565fe
indentd3q <vincentzq88@gmail.com>
2024-06-15 23:53:30 +0700

    Hmm binggung Desing

--- my-app/src/components/Blog.js
+++ my-app/src/components/Blog.js
@@ -21,7 +21,7 @@ const blogPosts = [
 const Blog = () => (
   <section id="blog" className="bg-white py-20">
     <div className="container mx-auto">
-      <h2 className="text-4xl font-bold text-center mb-12">My Blog</h2>
+      <h2 className="text-4xl font-bold text-center mb-12">Vincent's Blog</h2>
       <div className="flex flex-wrap justify-center">
         {blogPosts.map((post) => (
           <div key={post.id} className="blog-post w-full sm:w-1/2 md:w-1/3 xl:w-1/4 p-4">
