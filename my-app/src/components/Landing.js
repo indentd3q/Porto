41be39f118d7d75a53ba3eaf5dd0b16ff8ff9554
VincentKurniawan <133878185+indentd3q@users.noreply.github.com>
2024-11-04 08:36:23 +0700

    Update Routing

--- my-app/src/components/Landing.js
+++ my-app/src/components/Landing.js
@@ -29,7 +29,7 @@ const Landing = () => {
                     <p className="py-2 pb-6 md:pb-10 text-sm md:text-base">Hey there!! My name is Vincent and currently i'm an undergraduate student in Multimedia Nusantara University in Indonesia.</p>
                     <div className="flex flex-col md:flex-row items-center">
                         <Link to="/hire-me" className="text-white no-underline py-1 md:py-2 px-6 md:px-10 bg-purple-600 rounded-lg border-2 border-purple-600 font-medium transition duration-300 hover:text-purple-600 hover:bg-transparent mb-2 md:mb-0">Hire Me</Link>
-                        <Link to="/porto" className="text-purple-600 no-underline py-1 md:py-2 px-6 md:px-8 bg-transparent rounded-lg border-2 border-purple-600 font-medium transition duration-300 ml-0 md:ml-7 hover:text-white hover:bg-purple-600">See Projects</Link>
+                        <Link to="/projects" className="text-purple-600 no-underline py-1 md:py-2 px-6 md:px-8 bg-transparent rounded-lg border-2 border-purple-600 font-medium transition duration-300 ml-0 md:ml-7 hover:text-white hover:bg-purple-600">See Projects</Link>
                     </div>
                     <div className="absolute bottom-8 md:bottom-12 flex space-x-4 md:space-x-6">
                         <a href="https://www.linkedin.com/in/vincent-kurniawan" target="_blank" rel="noopener noreferrer" className="w-8 md:w-9 h-8 md:h-9 bg-purple-600 rounded-full flex justify-center items-center border border-white outline outline-2 outline-purple-600 transition duration-300 hover:translate-y-[-5px]">
